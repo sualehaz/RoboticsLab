cd2a2d76631a3e251437a40c2acb3abb1104226f
sualehaz <sualehazainab@hotmail.com>
2026-04-22 22:47:04 +0500

    Update Lab 6 - checkSelfCollision.m

--- Lab 6 - checkSelfCollision.m
+++ Lab 6 - checkSelfCollision.m
@@ -6,14 +6,18 @@ function isCollision = checkSelfCollision1(robot,initial_angles, final_angles)
     for i = 1:num_samples
         fraction = i / num_samples; 
         current_config = initial_angles + fraction * (final_angles - initial_angles);
-        collision_status = checkCollision(robot,current_config,"SkippedSelfCollisions", "adjacent");
-        
-        if any(collision_status)
+        collision_status = checkCollision(robot,current_config);
+
+        table = collisionBox(25, 25, 1);
+        table.Pose = trvec2tform([0, 0, -1]);
+
+        if any(collision_status) || any(checkCollision(robot, current_config, {table}))
             isCollision = true;
             fprintf('WARNING: Self-collision detected at path step %d!\n', i);
             fprintf('Dangerous Angles: [%.2f, %.2f, %.2f, %.2f]\n', current_config);
             break;
         end
+        show(robot, current_config, 'Collisions', 'on', 'Visuals', 'on'); drawnow;
     end
     
     if ~isCollision
