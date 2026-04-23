9767f257791e3fb98335b2f71801b7ead56739f5
sualehaz <sualehazainab@hotmail.com>
2026-04-23 11:56:36 +0500

    Update and rename Lab 6 - checkSelfCollision.m to Lab 6 - checkSelfCollision1.m

--- Lab 6 - checkSelfCollision1.m
+++ Lab 6 - checkSelfCollision1.m
@@ -17,7 +17,26 @@ function isCollision = checkSelfCollision1(robot,initial_angles, final_angles)
             fprintf('Dangerous Angles: [%.2f, %.2f, %.2f, %.2f]\n', current_config);
             break;
         end
-        show(robot, current_config, 'Collisions', 'on', 'Visuals', 'on'); drawnow;
+        show(robot, current_config, 'Collisions', 'on', 'Visuals', 'on', 'Frames','on'); 
+        
+        % Code for displaying an xyz axis on the end effector
+
+        hold on; 
+        
+        T_ee = getTransform(robot, current_config, 'link4');
+        
+        pos = T_ee(1:3, 4);
+        R = T_ee(1:3, 1:3);
+        
+        axis_len = 5; 
+        
+        % (X=Red, Y=Green, Z=Blue)
+        quiver3(pos(1), pos(2), pos(3), R(1,1), R(2,1), R(3,1), axis_len, 'r', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+        quiver3(pos(1), pos(2), pos(3), R(1,2), R(2,2), R(3,2), axis_len, 'g', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+        quiver3(pos(1), pos(2), pos(3), R(1,3), R(2,3), R(3,3), axis_len, 'b', 'LineWidth', 3, 'MaxHeadSize', 0.5);
+        
+        hold off;
+        drawnow;
     end
     
     if ~isCollision
