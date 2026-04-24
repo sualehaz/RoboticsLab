4d5d94328de4e1b505726d2f46d46c2ac266f8c0
sualehaz <sualehazainab@hotmail.com>
2026-04-24 12:28:05 +0500

    Update Milestone 3 - motion_pipeline.m

--- Milestone 3 - motion_pipeline.m
+++ Milestone 3 - motion_pipeline.m
@@ -1,46 +1,63 @@
 % PICK AND PLACE MOTION PIPELINE
-arb = Arbotix('port', 'COM3', 'nservos', 5);
+% arb = Arbotix('port', 'COM3', 'nservos', 5);
 
 % Starting from default position
 arb.setpos(1, 0, 100);
 arb.setpos(2, 0, 100);
 arb.setpos(3, 0, 100);
 arb.setpos(4, 0, 100);
+arb.setpos(5, 0, 100);
+
+x=12.7;
+y=-14.2;
+
+x_final = 11;
+y_final = 14;
 
 % Going to pre-grasp position (z = 8) CASE 1
-setting_pose(arb, -12.7, 14.2, 8)
+setting_pose(arb, x, y, 8)
 
-% Going to grasp position (z=4) CASE 1
-setting_pose(arb, -12.7, 14.2, 4)
+% Going to grasp position (z=6) CASE 1
+setting_pose(arb, x, y, 6)
 
 % Going to pre-grasp position (z = 10) CASE 2
-% setting_pose(arb, target_x, target_y, 10)
+ %setting_pose(arb, x, y, 10)
 
 % Going to grasp position (z=6) CASE 2
-% setting_pose(arb, target_x, target_y, 6)
+ %setting_pose(arb, x, y, 6)
 
-% Grasping the block (angle5 = pi/4) 
-arb.setpos(5, pi/4, 100);
+% Grasping the block (angle5 = 1.3) 
+arb.setpos(5, 1.3, 100);
 
 % Going to specified destination coordinates in pre-place position (z = 8)
 % CASE 1
-setting_pose(arb, 11.7, 11.3, 8)
+setting_pose(arb, x_final, y_final, 8)
 
 % Going to place position (z=4) CASE 1
-setting_pose(arb, 11.7, 11.3, 4)
+setting_pose(arb, x_final, y_final, 6)
 
+pause(1);
 % Going to specified destination coordinates in pre-place position (z = 10)
 % CASE 2
-% setting_pose(arb, target_x, target_y, 10)
+ %setting_pose(arb, x_final, y_final, 10)
 
 % Going to place position (z=6) CASE 2
-% setting_pose(arb, target_x, target_y, 6)
-
+% setting_pose(arb, x_final, y_final, 6)
+%pause(1);
 % Ungrasping the block (angle5 = 0)
-arb.setpos(5, 0, 100);
+ arb.setpos(5, 0, 100);
+ pause(1);
 
+ %going back to pre-place
+ setting_pose(arb, x_final, y_final, 8);
+ pause(1);
+
+ 
 % Robot going back to default position 
-arb.setpos(1, 0, 100);
 arb.setpos(2, 0, 100);
+arb.setpos(1, 0, 100);
 arb.setpos(3, 0, 100);
 arb.setpos(4, 0, 100);
+
+
+
