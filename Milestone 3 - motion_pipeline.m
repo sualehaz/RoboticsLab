4da6200428f2021e36b7c0f2af079fa1583f0268
sualehaz <sualehazainab@hotmail.com>
2026-04-25 15:12:14 +0500

    Update Milestone 3 - motion_pipeline.m

--- Milestone 3 - motion_pipeline.m
+++ Milestone 3 - motion_pipeline.m
@@ -1,63 +1,76 @@
 % PICK AND PLACE MOTION PIPELINE
 % arb = Arbotix('port', 'COM3', 'nservos', 5);
-
+function motion_pipeline
 % Starting from default position
+arb = Arbotix('port', 'COM3', 'nservos', 5);
 arb.setpos(1, 0, 100);
-arb.setpos(2, 0, 100);
+arb.setpos(2, pi/2, 100);
 arb.setpos(3, 0, 100);
 arb.setpos(4, 0, 100);
 arb.setpos(5, 0, 100);
 
-x=12.7;
-y=-14.2;
+[x_initial, y_initial, depth] = current_coordinates()
 
-x_final = 11;
-y_final = 14;
+x_final = 0;
+y_final = 15;
+
 
 % Going to pre-grasp position (z = 8) CASE 1
-setting_pose(arb, x, y, 8)
+err = setting_pose(arb, x_initial, y_initial, 8)
+
+if err == 1, return; end
 
 % Going to grasp position (z=6) CASE 1
-setting_pose(arb, x, y, 6)
+err = setting_pose(arb, x_initial, y_initial, 6)
+if err == 1, return; end
+
 
 % Going to pre-grasp position (z = 10) CASE 2
- %setting_pose(arb, x, y, 10)
+ % setting_pose(arb, x, y, 10)
 
 % Going to grasp position (z=6) CASE 2
- %setting_pose(arb, x, y, 6)
+ % setting_pose(arb, x, y, 8)
 
 % Grasping the block (angle5 = 1.3) 
 arb.setpos(5, 1.3, 100);
 
 % Going to specified destination coordinates in pre-place position (z = 8)
 % CASE 1
-setting_pose(arb, x_final, y_final, 8)
+err = setting_pose(arb, x_final, y_final, 8)
+if err == 1, return; end
+
 
 % Going to place position (z=4) CASE 1
-setting_pose(arb, x_final, y_final, 6)
+err = setting_pose(arb, x_final, y_final, 6)
+if err == 1, return; end
+
 
 pause(1);
 % Going to specified destination coordinates in pre-place position (z = 10)
 % CASE 2
- %setting_pose(arb, x_final, y_final, 10)
+% setting_pose(arb, x_final, y_final, 10)
 
 % Going to place position (z=6) CASE 2
-% setting_pose(arb, x_final, y_final, 6)
+% setting_pose(arb, x_final, y_final, 8)
 %pause(1);
 % Ungrasping the block (angle5 = 0)
- arb.setpos(5, 0, 100);
- pause(1);
+arb.setpos(5, 0, 100);
+pause(1);
 
- %going back to pre-place
- setting_pose(arb, x_final, y_final, 8);
- pause(1);
+%going back to pre-place
+setting_pose(arb, x_final, y_final, 10);
+if err == 1, return; end
 
+pause(1);
+
+x_initial
+y_initial
  
 % Robot going back to default position 
-arb.setpos(2, 0, 100);
+arb.setpos(2, pi/2, 100);
 arb.setpos(1, 0, 100);
 arb.setpos(3, 0, 100);
 arb.setpos(4, 0, 100);
 
-
+end
 
