084bb7fbb7317b411262b01acc94e989251e0dc8
sualehaz <sualehazainab@hotmail.com>
2026-04-28 11:07:14 +0500

    Create Milestone 4 - creating_masks.m

---
+++ Milestone 4 - creating_masks.m
@@ -0,0 +1,45 @@
+function [redMask, blueMask, greenMask, yellowMask] = creating_masks()
+
+[depth_img, rgb_img, depth_data] = depth_sensor();
+
+%% 1. Read Image
+imgRGB = rgb_img;
+figure; 
+imshow(imgRGB);
+title('Original RGB Image');
+
+%% 2. Convert to LAB
+imgLab = rgb2lab(imgRGB); % Switch to LAB space to make color separation easier
+[L,a,b] = imsplit(imgLab); % Break it down into lightness and color channels
+figure;
+imshow(imgLab);
+title ('LAB image');
+
+%% 3. Segment Yellow Block
+yellowMask = b > 30; % Isolating yellow using the 'b' channel (blue-yellow)
+yellowMask = bwareaopen(yellowMask, 200); % Getting rid of small noise or "speckles"
+figure;
+imshow(yellowMask);
+title ('yellow mask applied');
+
+%% 3. Segment Blue Block
+blueMask = b < -21; % Isolating yellow using the 'b' channel (blue-yellow)
+blueMask = bwareaopen(blueMask, 200); % Getting rid of small noise or "speckles"
+figure;
+imshow(blueMask);
+title ('blue mask applied');
+
+%% 3. Segment Red Block
+redMask = a > 31; % Isolating yellow using the 'b' channel (blue-yellow)
+redMask = bwareaopen(redMask, 200); % Getting rid of small noise or "speckles"
+figure;
+imshow(redMask);
+title ('red mask applied');
+
+%% 3. Segment Green Block
+greenMask = a < -17; % Isolating yellow using the 'b' channel (blue-yellow)
+greenMask = bwareaopen(greenMask, 200); % Getting rid of small noise or "speckles"
+figure;
+imshow(greenMask);
+title ('green mask applied');
+
