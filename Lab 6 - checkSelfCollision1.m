61a3ce6ada3d528ae27371c08f834f99b56f3124
sualehaz <sualehazainab@hotmail.com>
2026-05-07 13:05:59 +0500

    Update Lab 6 - checkSelfCollision1.m

--- Lab 6 - checkSelfCollision1.m
+++ Lab 6 - checkSelfCollision1.m
@@ -2,11 +2,16 @@ function isCollision = checkSelfCollision1(robot,initial_angles, final_angles)
 
     num_samples = 50; 
     isCollision = false;
-    
+    % 
+    % % fix for random error issue (jugadh XD) 
+    % if length(initial_angles) == 3
+    %     initial_angles = [initial_angles(:); 0];
+    % end
+
     for i = 1:num_samples
         fraction = i / num_samples; 
         current_config = initial_angles + fraction * (final_angles - initial_angles);
-        collision_status = checkCollision(robot,current_config);
+        collision_status = checkCollision(robot,current_config, 'SkippedSelfCollisions', 'adjacent');
 
         table = collisionBox(25, 25, 1);
         table.Pose = trvec2tform([0, 0, -1]);
@@ -17,26 +22,6 @@ function isCollision = checkSelfCollision1(robot,initial_angles, final_angles)
             fprintf('Dangerous Angles: [%.2f, %.2f, %.2f, %.2f]\n', current_config);
             break;
         end
-        show(robot, current_config, 'Collisions', 'on', 'Visuals', 'on', 'Frames','on'); 
-        
-        % Code for displaying an xyz axis on the end effector
-
-        hold on; 
-        
-        T_ee = getTransform(robot, current_config, 'link4');
-        
-        pos = T_ee(1:3, 4);
-        R = T_ee(1:3, 1:3);
-        
-        axis_len = 5; 
-        
-        % (X=Red, Y=Green, Z=Blue)
-        quiver3(pos(1), pos(2), pos(3), R(1,1), R(2,1), R(3,1), axis_len, 'r', 'LineWidth', 3, 'MaxHeadSize', 0.5);
-        quiver3(pos(1), pos(2), pos(3), R(1,2), R(2,2), R(3,2), axis_len, 'g', 'LineWidth', 3, 'MaxHeadSize', 0.5);
-        quiver3(pos(1), pos(2), pos(3), R(1,3), R(2,3), R(3,3), axis_len, 'b', 'LineWidth', 3, 'MaxHeadSize', 0.5);
-        
-        hold off;
-        drawnow;
     end
     
     if ~isCollision
