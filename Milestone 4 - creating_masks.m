cc0d6ca6020f9972fa3a34d2763659bad6cda7d5
sualehaz <sualehazainab@hotmail.com>
2026-05-08 18:33:06 +0500

    Update Milestone 4 - creating_masks.m

--- Milestone 4 - creating_masks.m
+++ Milestone 4 - creating_masks.m
@@ -1,9 +1,8 @@
-function [redMask, blueMask, greenMask, yellowMask] = creating_masks()
+function [redMask, blueMask, greenMask, yellowMask] = creating_masks(imgRGB)
 
-[depth_img, rgb_img, depth_data] = depth_sensor();
+
 
 %% 1. Read Image
-imgRGB = rgb_img;
 % figure; 
 % imshow(imgRGB);
 % title('Original RGB Image');
@@ -16,28 +15,28 @@ imgLab = rgb2lab(imgRGB); % Switch to LAB space to make color separation easier
 % title ('LAB image');
 
 %% 3. Segment Yellow Block
-yellowMask = b > 30; % Isolating yellow using the 'b' channel (blue-yellow)
+yellowMask = b > 40; % Isolating yellow using the 'b' channel (blue-yellow)
 yellowMask = bwareaopen(yellowMask, 200); % Getting rid of small noise or "speckles"
-% figure;
-% imshow(yellowMask);
-% title ('yellow mask applied');
+figure;
+imshow(yellowMask);
+title ('yellow mask applied');
 
 %% 3. Segment Blue Block
-blueMask = b < -30; % Isolating yellow using the 'b' channel (blue-yellow)
+blueMask = b < -25; % Isolating yellow using the 'b' channel (blue-yellow)
 blueMask = bwareaopen(blueMask, 200); % Getting rid of small noise or "speckles"
-figure;
-imshow(blueMask);
-title ('blue mask applied');
+% figure;
+% imshow(blueMask);
+% title ('blue mask applied');
 
 %% 3. Segment Red Block
 redMask = a > 28; % Isolating yellow using the 'b' channel (blue-yellow)
 redMask = bwareaopen(redMask, 200); % Getting rid of small noise or "speckles"
-% figure;
-% imshow(redMask);
-% title ('red mask applied');
+figure;
+imshow(redMask);
+title ('red mask applied');
 
 %% 3. Segment Green Block
-greenMask = a < -13; % Isolating yellow using the 'b' channel (blue-yellow)
+greenMask = a < -25; % Isolating yellow using the 'b' channel (blue-yellow)
 greenMask = bwareaopen(greenMask, 200); % Getting rid of small noise or "speckles"
 % figure;
 % imshow(greenMask);
