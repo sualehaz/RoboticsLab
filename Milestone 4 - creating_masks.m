9388f280468d41de01433ee3f1e11d013ae6e9f4
nevakumari <156394536+nevakumari@users.noreply.github.com>
2026-05-10 15:23:34 +0500

    Update Milestone 4 - creating_masks.m

--- Milestone 4 - creating_masks.m
+++ Milestone 4 - creating_masks.m
@@ -15,31 +15,29 @@ imgLab = rgb2lab(imgRGB); % Switch to LAB space to make color separation easier
 % title ('LAB image');
 
 %% 3. Segment Yellow Block
-yellowMask = b > 40; % Isolating yellow using the 'b' channel (blue-yellow)
+yellowMask = b > 42; % Isolating yellow using the 'b' channel (blue-yellow)
 yellowMask = bwareaopen(yellowMask, 200); % Getting rid of small noise or "speckles"
-figure;
-imshow(yellowMask);
-title ('yellow mask applied');
+% figure;
+% imshow(yellowMask);
+% title ('yellow mask applied');
 
 %% 3. Segment Blue Block
-blueMask = b < -25; % Isolating yellow using the 'b' channel (blue-yellow)
+blueMask = b < -24; % Isolating yellow using the 'b' channel (blue-yellow)
 blueMask = bwareaopen(blueMask, 200); % Getting rid of small noise or "speckles"
 % figure;
 % imshow(blueMask);
 % title ('blue mask applied');
 
 %% 3. Segment Red Block
-redMask = a > 28; % Isolating yellow using the 'b' channel (blue-yellow)
+redMask = a > 29; % Isolating yellow using the 'b' channel (blue-yellow)
 redMask = bwareaopen(redMask, 200); % Getting rid of small noise or "speckles"
-figure;
-imshow(redMask);
-title ('red mask applied');
+% figure;
+% imshow(redMask);
+% title ('red mask applied');
 
 %% 3. Segment Green Block
-greenMask = a < -25; % Isolating yellow using the 'b' channel (blue-yellow)
+greenMask = a < -19; % Isolating yellow using the 'b' channel (blue-yellow)
 greenMask = bwareaopen(greenMask, 200); % Getting rid of small noise or "speckles"
-% figure;
-% imshow(greenMask);
-% title ('green mask applied');
-
-
+figure;
+imshow(greenMask);
+title ('green mask applied');
